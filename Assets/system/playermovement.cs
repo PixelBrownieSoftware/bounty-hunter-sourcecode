ff5ed3841a00f75de1cb568441eba3cf77bcaa5a
Pixel Brownie Software <33553191+PixelBrownieSoftware@users.noreply.github.com>
2019-01-17 21:22:24 +0000

    Clean up

--- Assets/system/playermovement.cs
+++ Assets/system/playermovement.cs
@@ -1,4 +1,9 @@
-﻿using UnityEngine;
+/* 
+         * DEV'S COMMENTS 10/11/2017 - I honestly don't think this is a good way of handling movement, in Bounty Hunter 2
+         * I have unity's built-in function "vspeed = Input.GetAxisRaw("Vertical")"
+         */
+
+using UnityEngine;
 using System.Collections;
 
 public class playermovement : MonoBehaviour {
@@ -11,40 +16,8 @@ public class playermovement : MonoBehaviour {
     public Transform firept;
     public bool attacking;
     
-
-
     void Update()
     {
-
-        /* if (hspeed != 0 || vspeed != 0)
-         {
-             anim.SetBool("iswalking", true);
-             anim.SetFloat("inp_x", hspeed);
-             anim.SetFloat("inp_y", vspeed);
-
-         }
-         else {
-             anim.SetBool("iswalking", false);
-         }
-
-         if (Input.GetButtonDown("interact"))
-         {
-             anim.SetBool("ishiting", true);
-         }
-         else
-         {
-             anim.SetBool("ishiting", false);
-         }
-         */
-
-
-
-        //moving without rigidbody
-        /* 
-         * DEV'S COMMENTS 10/11/2017 - I honestly don't think this is a good way of handling movement, in Bounty Hunter 2
-         * I have unity's built-in function "vspeed = Input.GetAxisRaw("Vertical")"
-         */
-
         if (attacking == false)
         {
             if (Input.GetAxisRaw("Horizontal") < -0.5f)
@@ -71,18 +44,7 @@ public class playermovement : MonoBehaviour {
                 transform.position += new Vector3(0, vspeed, 0) * Time.deltaTime;
             }
         }
-
-        
-
-
-        // if (Input.GetAxisRaw("Vertical") < 0.5f || Input.GetAxisRaw("Vertical") > 0.5f)
-        //  {
-        //     transform.Translate(new Vector3(0f ,Input.GetAxisRaw("Vertical") * hspeed * Time.deltaTime, 0f));
-        //  }
-
-
     }
-
 
 }
 
